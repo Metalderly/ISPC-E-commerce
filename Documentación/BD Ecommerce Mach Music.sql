b00d4b6357d84af700eececde60682ac89274f15
Metalderly2023 <osvaldoeverdiaz@gmail.com>
2023-05-06 18:25:25 -0300

    Base de datos

---
+++ Documentación/BD Ecommerce Mach Music.sql
@@ -0,0 +1,27 @@
+create database Music_Ecommerce;
+
+use Music_Ecommerce;
+
+-- Tabla de Usuario
+
+create table Usuario (
+Id_U int,
+DNI int,
+Nombre varchar (45),
+Apellido varchar (45),
+Email varchar (45),
+Nro_Celular int,
+Pais varchar (45),
+Provincia varchar (45),
+Ciudad varchar (45),
+Codigo_Postal int,
+Pass int,
+Genero varchar (20),
+PRIMARY KEY (Id_U)
+) 
+
+
+
+
+
+
