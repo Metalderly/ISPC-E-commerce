bba7a1187386ad24e1a8fcd18b8fd480f03c2dec
valen <valentinogiannico04@gmail.com>
2023-06-04 19:51:22 -0300

    terimé con la implementacion de la api con los formularios y productos

--- Django/BDEcommerce/api/views.py
+++ Django/BDEcommerce/api/views.py
@@ -1,13 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from BaseCentral.models import Usuario , Producto
+from BaseCentral.models import Users , Producto
 from .serializers import UserSerializer, ProductSerializer 
 
 class UserView(APIView):
 
     def get(self, request):
-        model = Usuario.objects.all()
+        model = Users.objects.all()
         serializer = UserSerializer(model, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
@@ -17,6 +17,16 @@ class UserView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_404_NOT_FOUND)
+
+class UserByUsernameView(APIView):
+    def get(self, request, username, password):
+      try:
+        model = Users.objects.get(username=username, password=password)
+        serializer = UserSerializer(model)
+        if(Users.objects.filter(username=username, password=password).exists()):
+          return Response(serializer.data)
+      except:
+          return Response("Datos incorrectos")
 
 class ProductView(APIView):
     def get(self, request):
@@ -30,3 +40,14 @@ class ProductView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_404_NOT_FOUND)
+
+class ProductByUsernameView(APIView):
+    def get(self, request, username):
+        products = Producto.objects.filter(vendedor=Users.objects.get(username=username).id)
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+class ProductByIdView(APIView):
+    def get(self, request, id):
+        product = Producto.objects.get(id=id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data, status=status.HTTP_200_OK)
